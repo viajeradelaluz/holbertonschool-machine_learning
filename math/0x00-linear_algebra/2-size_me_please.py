b44b6dddbba87ca5c6aeeeeab679b7d3d4ccec02
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-03 23:54:23 -0500

    docs: module docs added

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""
+Write a function that calculates the shape of a matrix:
+  - You can assume elements in the same dimension are of the same type/shape
+  - The shape should be returned as a list of integers
+"""
+
 
 def matrix_shape(matrix):
-    """Calculates the shape of a matrix.
-    - You can assume elements in the same dimension are of the same type/shape
-    - The shape should be returned as a list of integers
-    """
+    """Calculates the shape of a matrix."""
     shape = []
     while type(matrix) == list and len(matrix) != 0:
         shape.append(len(matrix))
