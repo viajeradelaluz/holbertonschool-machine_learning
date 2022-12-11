89332ab29b65a5c859d972b9768d0fd7f550ab82
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-11 14:34:23 -0500

    feat: matrix multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a function that performs matrix multiplication:
+- You can assume that mat1 and mat2 are numpy.ndarrays
+- You are not allowed to use any loops or conditional statements
+- You may use: import numpy as np
+- You can assume that mat1 and mat2 are never empty
+"""
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication"""
+    return mat1 @ mat2
