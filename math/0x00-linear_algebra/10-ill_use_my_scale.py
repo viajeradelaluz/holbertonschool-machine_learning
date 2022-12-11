9a05a672a119c8aab0703a5a0ec972263457d7f1
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-11 14:11:15 -0500

    feat: numpy array.shape()

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a function  that calculates the shape of a numpy.ndarray:
+- You are not allowed to use any loops or conditional statements
+- You are not allowed to use try/except statements
+- The shape should be returned as a tuple of integers
+"""
+
+
+def np_shape(matrix):
+    """Calculate the shape of a numpy.ndarray."""
+    return matrix.shape
