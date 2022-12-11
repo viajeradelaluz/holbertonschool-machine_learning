05fc96105b4a745e0de504ca7c46eb0809c15169
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-11 17:03:10 -0500

    feat: scale change in plot

--- math/0x01-plotting/1-scatter.py
+++ math/0x01-plotting/1-scatter.py
@@ -8,7 +8,7 @@ np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
 
-plt.scatter(x, y, c="m", marker=".")
+plt.scatter(x, y, c="m", marker=".")  # type: ignore
 plt.title("Men's Height vs Weight")
 plt.xlabel("Height (in)")
 plt.ylabel("Weight (lbs)")
