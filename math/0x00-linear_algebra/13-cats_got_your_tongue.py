b3e44ce1581ada43621050d3b72909cc90fdc3cf
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-11 14:25:03 -0500

    feat: matrix concatenation with numpy

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Write a function that concatenates two matrices along a specific axis:
+- You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
+- You must return a new numpy.ndarray
+- You are not allowed to use any loops or conditional statements
+- You may use: import numpy as np
+- You can assume that mat1 and mat2 are never empty
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis"""
+    return np.concatenate((mat1, mat2), axis)
