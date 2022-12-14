9350b9d0c6fa2342b57c9d6bb2c88a9d88fc12ba
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-14 18:23:01 -0500

    feat: cdf of poisson

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -31,9 +31,21 @@ class Poisson:
         if k < 0:
             return 0
 
-        # Ref. [Distribución de Poisson](https://youtu.be/PMX75m4-s9A)
+        # Ref. Distribución de Poisson PMF: https://youtu.be/PMX75m4-s9A
         e = 2.7182818285
         k_factorial = 1
         for i in range(1, k + 1):
             k_factorial *= i
         return (e**-self.lambtha) * (self.lambtha**k) / k_factorial
+
+    def cdf(self, k):
+        """Calculates the CDF for a given number of successes."""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+
+        # Ref. Distribución de Poisson CDF: https://youtu.be/x9jF11I5x-g
+        e = 2.7182818285
+        cdf = [self.pmf(i) for i in range(k + 1)]
+        return sum(cdf)
