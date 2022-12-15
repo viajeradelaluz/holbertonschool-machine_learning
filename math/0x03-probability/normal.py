486c12244ae3c36d8cf46d0033f25468eeb69cee
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-12-14 19:28:10 -0500

    feat: z score & x value

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -27,3 +27,11 @@ class Normal:
             self.mean = sum(data) / len(data)
             sd = sum([(x - self.mean) ** 2 for x in data]) / len(data)
             self.stddev = sd**0.5
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value."""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score."""
+        return z * self.stddev + self.mean
