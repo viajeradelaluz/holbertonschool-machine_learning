ca69b64085f74ee261483bed16c986af353da23a
Johanna Alfonso <jonna.alfonso@gmail.com>
2023-01-13 21:37:27 -0500

    feat: adam optimization algorithm

---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Updates a variable using the Adam optimization algorithm
+"""
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """Updates a variable in place using the Adam optimization algorithm.
+
+    :param alpha: the learning rate
+    :param beta1: the weight used for the first moment
+    :param beta2: the weight used for the second moment
+    :param epsilon: small number to avoid division by zero
+    :param var: a np array with the variable to be updated
+    :param grad: a np array with the gradient of var
+    :param v: the previous first moment of var
+    :param s: the previous second moment of var
+    :param t: the time step used for bias correction
+
+    :return: the updated variable, the new first moment, and the new second
+    moment, respectively
+    """
+    vdw = beta1 * v + (1 - beta1) * grad
+    sdw = beta2 * s + (1 - beta2) * grad**2
+
+    vdw_corrected = vdw / (1 - beta1**t)
+    sdw_corrected = sdw / (1 - beta2**t)
+
+    var = var - alpha * (vdw_corrected / (np.sqrt(sdw_corrected) + epsilon))
+
+    return var, vdw, sdw
