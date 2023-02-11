a723c60c9e96e1940314557e6b8ded8637091ce4
Johanna Alfonso <jonna.alfonso@gmail.com>
2023-02-11 15:24:53 -0500

    fix: imports with single quotes

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -6,8 +6,8 @@ Learning for Image Recognition (2015).
 
 import tensorflow.keras as K
 
-identity_block = __import__("2-identity_block").identity_block
-projection_block = __import__("3-projection_block").projection_block
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
